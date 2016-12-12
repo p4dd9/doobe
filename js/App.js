9a0e11573820d310f1f54e6a4a7db3ebfe8c925a
Johannes Obermair <johannes.obermair@gmail.com>
2016-12-12 09:23:01 +0100

    Moved project to git root

---
+++ js/App.js
@@ -0,0 +1,22 @@
+import User from "./User";
+$(document).ready(function () {
+
+    let $task = $('.task');
+
+    $task.on('swiperight', destroyTarget);
+
+    function destroyTarget() {
+        this.remove();
+    }
+});
+
+lists = document.getElementsByClassName('task');
+let g = 1;
+for (let i = 0; i < lists.length; i++) {
+
+    lists[i].style.backgroundColor = 'rgba(0,90,100,' + g + ')';
+
+    g -= (1 / lists.length);
+}
+
+let user = new User();
---
+++ js/Task.js
@@ -0,0 +1,33 @@
+export default class Task {
+    constructor(dueDate = Date.now(), taskText = "taskText", workload = 100) {
+        this._id = this.generateID();
+        this.createdDate = Date.now();
+        this.dueDate = dueDate;
+        this.taskText = taskText;
+        this.workload = workload; // fibonacci 1, 2, 3, 5, 8, 13
+    }
+
+    getTimeSpan(dueDate = this.dueDate) {
+        return dueDate - this.createdDate;
+    }
+
+    static generateID() {
+        /*let numPattern = /[0-9]/g;
+        return parseInt((numPattern.exec(new Date().toISOString())), 10);*/
+
+        return Date.now();
+    }
+
+    /***
+     * calculates the amout of xp depending on the amount of workload
+     * and when the task has been finish (the earlier, the better)
+     * @returns {number}
+     */
+    calcXpGain() {
+        let elapsedTime = this.getTimeSpan(Date.now());
+        let originalTimeSpan = this.getTimeSpan();
+        let timeBonus = (originalTimeSpan / elapsedTime);
+
+        return timeBonus * this.workload; // possible: add random factor
+    }
+};
